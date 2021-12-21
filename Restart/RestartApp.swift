ca1bbef346a84288b9ddd958bb4c396bc5049376
Ivan Galarza <ivang@bluetrailsoft.com>
2021-12-21 14:01:58 -0400

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ivan on 21/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
